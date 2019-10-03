3f134f1c34ec309865d162a7a670a11f279d3266
Alice Balard <alice.cam.balard@gmail.com>
2019-10-03 10:32:06 +0200

    Up after adding categorical analysis for review

--- code/D_Models_parasiteLoads.R
+++ code/D_Models_parasiteLoads.R
@@ -55,6 +55,34 @@ plotInfWorms <- plotInfWorms +
     labels = labels,
     paste("Pinworm count"))
 
+### NB. Arbitrary cut. Just for validation to review. Not meaningful!
+data2cut_pinworms <- pinwormsdata_bal[pinwormsdata_bal$Aspiculuris_Syphacia >=1,]
+
+# arbitrary catagories HI: 0-0.2 / 0.2-0.6 / 0.6-1
+data2cut_pinworms$hybridCat <- "hybrids"
+data2cut_pinworms$hybridCat[data2cut_pinworms$HI <= 0.2] <- "Mmd"
+data2cut_pinworms$hybridCat[data2cut_pinworms$HI >= 0.6] <- "Mmm"
+data2cut_pinworms$hybridCat <- factor(data2cut_pinworms$hybridCat, levels = c("Mmd", "hybrids", "Mmm"))
+
+table(data2cut_pinworms$hybridCat)
+
+data2cut_pinworms %>% 
+  dplyr::group_by(hybridCat) %>%
+  dplyr::summarize(meanInt = mean(Aspiculuris_Syphacia))
+
+mod1 <- glm.nb(Aspiculuris_Syphacia ~ hybridCat, data = data2cut_pinworms)
+anova(mod1, test = "LRT")
+
+library(ggeffects)
+dat <- ggpredict(mod1, terms = "hybridCat")
+
+ggplot(dat, aes(x = x, y = predicted)) + 
+  geom_point() +
+  geom_errorbar(aes(ymin =conf.low, ymax= conf.high), width = 0.3) +
+  scale_x_continuous(labels = c("Mmd", "", "hybrids", "", "Mmm"), name = "") +
+  scale_y_continuous(name = "predicted count") +
+  ggtitle("Pinworms intensity")
+
 # qPCR Eimeria
 
 # intensity : delta_ct_max_MminusE+5 to have it positive (in DataPReparation)
@@ -114,4 +142,45 @@ plotInfEimeria <- parasiteLoad::bananaPlot(
     expression(
       paste("Eimeria intensity (", Delta, "Ct Mouse - Eimeria)")))
 plotInfEimeria
-    
+
+### NB. Arbitrary cut. Just for validation to review. Not meaningful!
+data2cut_eimeria <- qpcr_intensity_data
+
+data2cut_eimeria$shiftedDeltaCt == data2cut_eimeria$delta_ct_max_MminusE + 6 + shift
+
+mean(data2cut_eimeria$shiftedDeltaCt)
+mean(data2cut_eimeria$delta_ct_max_MminusE) + 6 + shift
+
+#arbitrary catagories HI: 0-0.2 / 0.2-0.6 / 0.6-1
+data2cut_eimeria$hybridCat <- "hybrids"
+data2cut_eimeria$hybridCat[data2cut_eimeria$HI <= 0.2] <- "Mmd"
+data2cut_eimeria$hybridCat[data2cut_eimeria$HI >= 0.6] <- "Mmm"
+data2cut_eimeria$hybridCat <- factor(data2cut_eimeria$hybridCat, levels = c("Mmd", "hybrids", "Mmm"))
+
+table(data2cut_eimeria$hybridCat)
+
+data2cut_eimeria %>% 
+  dplyr::group_by(hybridCat) %>%
+  dplyr::summarize(meanInt = mean(delta_ct_max_MminusE))
+
+data2cut_eimeria %>% 
+  dplyr::group_by(hybridCat) %>%
+  dplyr::summarize(meanInt = mean(shiftedDeltaCt) - 6 - shift)
+
+mod2 <- survreg(Surv(shiftedDeltaCt)~hybridCat, data = data2cut_eimeria, dist="weibull")
+anova(mod2)
+
+library(ggeffects)
+dat <- ggpredict(mod2, terms = "hybridCat")
+
+# to come back to delta Ct original
+dat$predicted <- dat$predicted - 6 - shift
+dat$conf.low <- dat$conf.low - 6 - shift
+dat$conf.high <- dat$conf.high - 6 - shift
+
+ggplot(dat, aes(x = x, y = predicted)) + 
+  geom_point() +
+  geom_errorbar(aes(ymin =conf.low, ymax= conf.high), width = 0.3) +
+  scale_x_continuous(labels = c("Mmd", "", "hybrids", "", "Mmm"), name = "") +
+  scale_y_continuous(name = "predicted deltaCt") +
+  ggtitle("Eimeria intensity")
